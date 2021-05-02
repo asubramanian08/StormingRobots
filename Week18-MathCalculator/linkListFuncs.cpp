2a131ba202abcef199a7855780c135dbbaca3d7b
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-05-02 17:27:54 -0400

    final place of files

---
+++ Week18-MathCalculator/linkListFuncs.cpp
@@ -0,0 +1,45 @@
+#include "linkListFuncsHeader.h"
+#include <stdlib.h>
+stack *push(stack *start, int storage)
+{
+    stack *newStart = (stack *)malloc(sizeof(stack));
+    newStart->storage = storage;
+    newStart->next = start;
+    return newStart;
+}
+double pop(stack **start)
+{
+    int storage;
+    storage = (*start)->storage;
+    struct stack *old = *start;
+    (*start) = (*start)->next;
+    free(old);
+    return storage;
+}
+queue *enque(queue *start, int storage)
+{
+    queue *current;
+    queue *newEnd = (queue *)malloc(sizeof(queue));
+    newEnd->next = NULL;
+    newEnd->storage = storage;
+    if (start != NULL)
+    {
+        for (current = start;
+             current->next != NULL;
+             current = current->next)
+            ;
+        current->next = newEnd;
+    }
+    else
+        start = newEnd;
+    return start;
+}
+double deque(queue **start)
+{
+    int storage;
+    storage = (*start)->storage;
+    queue *old = *start;
+    (*start) = (*start)->next;
+    free(old);
+    return storage;
+}
